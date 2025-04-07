7fabca91debcb5ed0c177d153be472b12fce279b
operatorultra <105389680+operatorultra@users.noreply.github.com>
2025-04-07 09:05:52 +0200

    UPDATE VERCEL CONFIG 2

--- remix.config.js
+++ remix.config.js
@@ -1,6 +1,8 @@
 // remix.config.js
 module.exports = {
 	serverModuleFormat: "esm", // Ensure ESM format
+	serverBuildPath: "build/server/index.js",
+	publicPath: "/build/",
 	future: {
 		unstable_cssModules: true, // Enable CSS modules support
 		unstable_cssSideEffectImports: true,
